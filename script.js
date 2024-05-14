f0ff0edf706391f25b3058c058d72be0c8daf532
Øystein Håberg <islandstone89@gmail.com>
2024-05-14 20:42:35 +0200

    * Linked to js file * Added show and hide utility classes * Added styles on aria-expanded="true" * Changed font-weight on .header__link-container * Styled and positioned .navbar__list-wrapper

--- script.js
+++ script.js
@@ -1,5 +1,19 @@
 
-/* Seelct the open menu button */
+/* Select the navbar buttons */
+let navbarButtons = document.querySelectorAll(".js-navbar__button");
+
+
+
+/* Loop through each of the buttons and assign Event Listener and arrow function */
+navbarButtons.forEach(button  => {
+    button.addEventListener("click", (e) => {
+        button.setAttribute("aria-expanded", "true")
+    } )
+});
+
+
+
+/* Select the open menu button */
 let openMenuBtn = document.querySelector(".js-open-menu-btn");
 
 /* Seelct the close menu button */
@@ -8,4 +22,19 @@ let closeMenuBtn = document.querySelector(".js-close-menu-btn");
 /* Create function that opens the menu */
 function openMenu() {
     openMenuBtn.setAttribute("aria-expanded", "true")
+    openMenuBtn.classList.toggle("hide")
+    closeMenuBtn.classList.toggle("show")
 }
+
+/* Create function that closes the menu */
+function closeMenu() {
+    openMenuBtn.setAttribute("aria-expanded", "false")
+    openMenuBtn.classList.toggle("hide")
+    closeMenuBtn.classList.toggle("show")
+}
+
+/* Add Event Listener on open menu button */
+openMenuBtn.addEventListener("click", openMenu)
+
+/* Add Event Listener on close menu button */
+closeMenuBtn.addEventListener("click", closeMenu)
